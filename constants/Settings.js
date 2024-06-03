5f1f627a3c523f8deccc2a852be4e6294e944ae7
Odyrac <odyrac@gmail.com>
2024-06-04 00:07:37 +0200

    a

--- constants/Settings.js
+++ constants/Settings.js
@@ -3,5 +3,5 @@ export default {
     // in debug mode, the app will skip the chrono, allow any input and skip timer on buttons
 
     serverUrl: 'https://budiscovery.hlly.fr/api/',
-    files: ['json/app.json', 'json/appEN.json', 'json/quiz.json', 'json/quizEN.json', 'json/scenario1.json', 'json/scenario1EN.json', 'json/scenario2.json', 'json/scenario2EN.json'],
+    files: ['json/app.json', 'json/appEN.json', 'json/quiz.json', 'json/quizEN.json', 'json/scenario1.json', 'json/scenario1EN.json', 'json/scenario2.json', 'json/scenario2EN.json', 'json/scenario3.json', 'json/scenario3EN.json'],
 }
